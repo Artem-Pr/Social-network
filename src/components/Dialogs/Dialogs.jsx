505fcb8308b6fa464524cda17132c3979123704b
Artem Priadkin <artem.priadkin@paxful.com>
2019-10-12 17:05:02 +0300

    Lesson 29

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,23 +5,8 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    // let dialogs = [
-    //     { id: 1, name: 'Dimych' },
-    //     { id: 2, name: 'Andrey' },
-    //     { id: 3, name: 'Sveta' },
-    //     { id: 4, name: 'Sasha' },
-    //     { id: 5, name: 'Viktor' },
-    //     { id: 6, name: 'Valera' }
-    // ]
-
-    // let messages = [
-    //     { id: 1, message: 'Hi' },
-    //     { id: 2, message: 'How are you?' },
-    //     { id: 3, message: 'Yo' }
-    // ]
-
-    let dialogsElements = props.dialogs.map(({id, name}) => <DialogItem name={name} id={id} />); //destructuring
-    let messagesElements = props.messages.map(m => <Message message={m.message} />);
+    let dialogsElements = props.state.dialogs.map(({id, name}) => <DialogItem name={name} id={id} />); //destructuring
+    let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
 
     return (
         <div className={s.dialogs}>
