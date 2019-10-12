41d7310a11cc6f17d9ed1e82f1d8e54c7c6bbc9f
Artem Priadkin <artem.priadkin@paxful.com>
2019-10-12 17:56:12 +0300

    Lesson 30, Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
@@ -8,7 +8,6 @@ import Dialogs from './components/Dialogs/Dialogs';
 
 function App(props) {
   return (
-    <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Navbar />
@@ -24,7 +23,6 @@ function App(props) {
               state={props.state.profilePage} /> }/>
         </div>
       </div>
-    </BrowserRouter>
   );
 }
 
