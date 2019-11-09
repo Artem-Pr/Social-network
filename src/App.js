171d07010a051ff3a055cac8a0d3fc0fc1ac65db
Artem Priadkin <artem.priadkin@paxful.com>
2019-11-09 22:09:42 +0200

    Lesson 49

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from "./components/Users/UsersContainer";
 
 function App(props) {
   debugger;
@@ -20,6 +21,8 @@ function App(props) {
             render={ () => <DialogsContainer /> } />
           <Route path="/profile"
             render={ () => <Profile /> }/>
+          <Route path="/users"
+            render={ () => <UsersContainer /> }/>
         </div>
       </div>
   );
---
+++ src/redux/users-reducer.js
@@ -0,0 +1,41 @@
+import avatar1 from '../Avatars/avatar1.png';
+import avatar5 from '../Avatars/avatar5.png';
+import avatar3 from '../Avatars/avatar3.png';
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
+
+let initialState = {
+    users: []
+};
+
+const usersReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FOLLOW:
+            return  {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) return {...u, followed: true};
+                    return u;
+                })
+            };
+        case UNFOLLOW:
+            return  {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) return {...u, followed: false};
+                    return u;
+                })
+            };
+        case SET_USERS:
+            return {...state, users: [...state.users, ...action.users]};
+        default:
+            return state;
+    }
+};
+
+export const followAC = (userId) => ({type: FOLLOW, userId});
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
+
+export default usersReducer;
